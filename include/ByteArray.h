c207d356fde0a5db14ac1eedc9a71f658f3e9749
Artem Zakharenko <zakhart1234567890@gmail.com>
2022-05-28 19:51:35 +0300

    Add RAMByteArray

--- include/ByteArray.h
+++ include/ByteArray.h
@@ -3,9 +3,29 @@
 
 class ByteArray {
 public:
-    virtual void append(const std::vector<char> &bytes) = 0;
-    virtual std::vector<char> read(int l, int r) = 0; //[l, r) -- semi-interval
+    virtual void append(const std::vector<unsigned char> &bytes) = 0;
+    virtual std::vector<unsigned char> read(int l, int r) = 0; //[l, r) -- semi-interval
+    virtual std::size_t size() = 0;
 
     virtual ~ByteArray() = default;
 };
 
+
+class RAMByteArray : public  ByteArray {
+private:
+    std::vector<unsigned char> byte_array;
+public:
+
+    void append(const std::vector<unsigned char> &bytes) override {
+        byte_array.insert(byte_array.end(), bytes.begin(), bytes.end());
+    }
+
+    std::vector<unsigned char> read(int l, int r) override {
+        return {byte_array.begin() + l, byte_array.begin() + r};
+    }
+
+    std::size_t size() override {
+        return byte_array.size();
+    }
+
+};
--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,23 @@
 #include "doctest.h"
 #include "FileManager.h"
+#include <memory>
 
 TEST_CASE("Simple test") {
     FileType ft(FilePurpose::SST, 4);
     CHECK(ft.get_sst_level() == 4);
+}
+
+TEST_CASE("RAMByteArray") {
+    RAMByteArray ram_arr;
+    ByteArray &arr = ram_arr;
+    CHECK(arr.size() == 0);
+    arr.append({0, 1, 2, 3});
+    CHECK(arr.size() == 4);
+    CHECK(arr.read(0, 1) == std::vector<unsigned char>({0}));
+    CHECK(arr.read(0, 4) == std::vector<unsigned char>({0, 1, 2, 3}));
+    CHECK(arr.read(3, 4) == std::vector<unsigned char>({3}));
+    CHECK(arr.read(3, 4) == std::vector<unsigned char>({3}));
+    arr.append({3, 4, 5});
+    CHECK(arr.size() == 7);
+    CHECK(arr.read(3, 7) == std::vector<unsigned char>({3, 3, 4, 5}));
 }
