cd6eeba89a4d105d713ccf74ed44ea38a6976b1c
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2022-06-04 12:48:59 +0300

    doesnt link xxhash

--- include/Core.h
+++ include/Core.h
@@ -1,13 +1,13 @@
-#pragma once 
+#pragma once
 
 #include <array>
 #include <cstddef>
 
 namespace kvaaas {
-	constexpr std::size_t KEY_SIZE_BITS = 128; // TODO read in config
-    constexpr std::size_t KEY_SIZE_BYTES = KEY_SIZE_BITS / 8; // TODO read in config
-	constexpr std::size_t VALUE_SIZE_BYTES = 2 * (1 << 10);
-	using KeyType = std::array<std::byte, KEY_SIZE_BYTES>;
-	using ValueType = std::array<std::byte, VALUE_SIZE_BYTES>;
-    using ByteType = std::byte;
-}
+constexpr std::size_t KEY_SIZE_BITS = 128;                // TODO read in config
+constexpr std::size_t KEY_SIZE_BYTES = KEY_SIZE_BITS / 8; // TODO read in config
+constexpr std::size_t VALUE_SIZE_BYTES = 2 * (1 << 10);
+using KeyType = std::array<std::byte, KEY_SIZE_BYTES>;
+using ValueType = std::array<std::byte, VALUE_SIZE_BYTES>;
+using ByteType = std::byte;
+} // namespace kvaaas
