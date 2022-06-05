9dbcc30fdc28acb1b895b38b48f6d2e2fa700e34
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2022-06-05 21:40:05 +0300

    Tmp

--- include/MemoryManager.h
+++ include/MemoryManager.h
@@ -1,101 +1,185 @@
 #pragma once
-#include <vector>
-#include <optional>
-#include <cassert>
-#include <map>
 #include "ByteArray.h"
-
+#include "json.hpp"
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <optional>
+#include <vector>
 
 namespace kvaaas {
 
-enum class MemoryPurpose {
-    KVS = 0,
-    SST = 1,
-    SKIP_LIST_UL = 2,
-    SKIP_LIST_BL = 3
+enum MemoryPurpose {
+  BEGIN=0,
+  KVS = 0,
+  SST = 1,
+  SKIP_LIST_UL = 2,
+  SKIP_LIST_BL = 3,
+  SKIP_LIST_UL_H = 4,
+  END=5,
 };
+
+inline std::string to_string(MemoryPurpose p) {
+  switch (p) {
+  case MemoryPurpose::KVS:
+    return "_kvs";
+  case MemoryPurpose::SST:
+    return "_sst";
+  case MemoryPurpose::SKIP_LIST_UL:
+    return "_skip_list_ul";
+  case MemoryPurpose::SKIP_LIST_BL:
+    return "_skip_list_bl";
+  case MemoryPurpose::SKIP_LIST_UL_H:
+    return "_skip_list_ul_h";
+  default:
+    std::cerr << "Unreachable! Incorrect MemoryPurpose!";
+  }
+}
 
 class MemoryType {
 private:
-    MemoryPurpose mp;
-    std::optional<std::size_t> sst_level;
+  MemoryPurpose mp;
+  std::optional<std::size_t> sst_level;
+
 public:
-    explicit MemoryType(MemoryPurpose mp_, std::optional<std::size_t> sst_level_ = {});
+  explicit MemoryType(MemoryPurpose mp_,
+                      std::optional<std::size_t> sst_level_ = {});
 
-    [[nodiscard]] MemoryPurpose get_memory_purpose() const;
+  [[nodiscard]] MemoryPurpose get_memory_purpose() const;
 
-    [[nodiscard]] std::size_t get_sst_level() const;
+  [[nodiscard]] std::size_t get_sst_level() const;
 
-    [[nodiscard]] bool has_sst_level() const;
-
+  [[nodiscard]] bool has_sst_level() const;
 };
 
 bool cmp_memory_type(MemoryType memory_type1, MemoryType memory_type2);
-using cmp_memory_type_type = bool(*)(MemoryType, MemoryType);
+using cmp_memory_type_type = bool (*)(MemoryType, MemoryType);
 
 class MemoryManager {
 public:
-    virtual ByteArrayPtr get_file(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) = 0;
+  virtual ByteArrayPtr get_file(MemoryPurpose memory_purpose,
+                                std::optional<std::size_t> sst_level) = 0;
 
-    virtual ByteArrayPtr create_file(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) = 0;
+  virtual ByteArrayPtr create_file(MemoryPurpose memory_purpose,
+                                   std::optional<std::size_t> sst_level) = 0;
 
-    virtual ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) = 0;
+  virtual ByteArrayPtr
+  start_overwrite(MemoryPurpose memory_purpose,
+                  std::optional<std::size_t> sst_level) = 0;
 
-    virtual void end_overwrite(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) = 0;
+  virtual void end_overwrite(MemoryPurpose memory_purpose,
+                             std::optional<std::size_t> sst_level) = 0;
 
-    virtual void remove(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) = 0;
+  virtual void remove(MemoryPurpose memory_purpose,
+                      std::optional<std::size_t> sst_level) = 0;
 
-    virtual ByteArrayPtr get_file(MemoryPurpose memory_purpose) = 0;
+  virtual ByteArrayPtr get_file(MemoryPurpose memory_purpose) = 0;
 
-    virtual ByteArrayPtr create_file(MemoryPurpose memory_purpose) = 0;
+  virtual ByteArrayPtr create_file(MemoryPurpose memory_purpose) = 0;
 
-    virtual ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose) = 0;
+  virtual ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose) = 0;
 
-    virtual void end_overwrite(MemoryPurpose memory_purpose) = 0;
+  virtual void end_overwrite(MemoryPurpose memory_purpose) = 0;
 
-    virtual void remove(MemoryPurpose memory_purpose) = 0;
+  virtual void remove(MemoryPurpose memory_purpose) = 0;
 
-    virtual ~MemoryManager() = default;
+  virtual ~MemoryManager() = default;
 };
-
-
 
 class RAMMemoryManager : public MemoryManager {
 private:
-    std::map<MemoryType, ByteArrayPtr, cmp_memory_type_type> memory{cmp_memory_type};
-    std::map<MemoryType, ByteArrayPtr, cmp_memory_type_type> memory_to_overwrite{cmp_memory_type};
+  std::map<MemoryType, ByteArrayPtr, cmp_memory_type_type> memory{
+      cmp_memory_type};
+  std::map<MemoryType, ByteArrayPtr, cmp_memory_type_type> memory_to_overwrite{
+      cmp_memory_type};
+
 public:
-    RAMMemoryManager() = default;
-    RAMMemoryManager(const RAMMemoryManager &) = delete;
-    RAMMemoryManager &operator=(const RAMMemoryManager &) = delete;
-    RAMMemoryManager(RAMMemoryManager &&) = default;
-    RAMMemoryManager &operator=(RAMMemoryManager &&) = default;
+  RAMMemoryManager() = default;
+  RAMMemoryManager(const RAMMemoryManager &) = delete;
+  RAMMemoryManager &operator=(const RAMMemoryManager &) = delete;
+  RAMMemoryManager(RAMMemoryManager &&) = default;
+  RAMMemoryManager &operator=(RAMMemoryManager &&) = default;
 
+  ByteArrayPtr get_file(MemoryPurpose memory_purpose,
+                        std::optional<std::size_t> sst_level) override;
 
-    ByteArrayPtr get_file(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) override;
+  ByteArrayPtr create_file(MemoryPurpose memory_purpose,
+                           std::optional<std::size_t> sst_level) override;
+  ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose,
+                               std::optional<std::size_t> sst_level) override;
 
-    ByteArrayPtr create_file(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) override;
+  void end_overwrite(MemoryPurpose memory_purpose,
+                     std::optional<std::size_t> sst_level) override;
 
-    ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) override;
+  void remove(MemoryPurpose memory_purpose,
+              std::optional<std::size_t> sst_level) override;
 
-    void end_overwrite(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) override;
+  ByteArrayPtr get_file(MemoryPurpose memory_purpose) override;
 
-    void remove(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) override;
+  ByteArrayPtr create_file(MemoryPurpose memory_purpose) override;
 
+  ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose) override;
 
-    ByteArrayPtr get_file(MemoryPurpose memory_purpose) override;
+  void end_overwrite(MemoryPurpose memory_purpose) override;
 
-    ByteArrayPtr create_file(MemoryPurpose memory_purpose) override;
+  void remove(MemoryPurpose memory_purpose) override;
 
-    ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose) override;
-
-    void end_overwrite(MemoryPurpose memory_purpose) override;
-
-    void remove(MemoryPurpose memory_purpose) override;
-
-
-    ~RAMMemoryManager() noexcept override;
-
+  ~RAMMemoryManager() noexcept override;
 };
 
-}
+struct Upload{};
+
+class FileMemoryManager : public MemoryManager {
+private:
+  std::map<MemoryType, ByteArrayPtr, cmp_memory_type_type> memory{
+      cmp_memory_type};
+  std::map<MemoryType, ByteArrayPtr, cmp_memory_type_type> memory_to_overwrite{
+      cmp_memory_type};
+
+public:
+  FileMemoryManager(std::string root);
+  FileMemoryManager(Upload, std::string root);
+  FileMemoryManager(nlohmann::json mem_json, nlohmann::json mem_to_ov, std::string root);
+  FileMemoryManager(const FileMemoryManager &) = delete;
+  FileMemoryManager &operator=(const FileMemoryManager &) = delete;
+  FileMemoryManager(FileMemoryManager &&) = default;
+  FileMemoryManager &operator=(FileMemoryManager &&) = default;
+
+  ByteArrayPtr get_file(MemoryPurpose memory_purpose,
+                        std::optional<std::size_t> sst_level) override;
+
+  ByteArrayPtr create_file(MemoryPurpose memory_purpose,
+                           std::optional<std::size_t> sst_level) override;
+
+  ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose,
+                               std::optional<std::size_t> sst_level) override;
+
+  void end_overwrite(MemoryPurpose memory_purpose,
+                     std::optional<std::size_t> sst_level) override;
+
+  void remove(MemoryPurpose memory_purpose,
+              std::optional<std::size_t> sst_level) override;
+
+  ByteArrayPtr get_file(MemoryPurpose memory_purpose) override;
+
+  ByteArrayPtr create_file(MemoryPurpose memory_purpose) override;
+
+  ByteArrayPtr start_overwrite(MemoryPurpose memory_purpose) override;
+
+  void end_overwrite(MemoryPurpose memory_purpose) override;
+
+  void remove(MemoryPurpose memory_purpose) override;
+
+  ~FileMemoryManager() noexcept override;
+
+  static FileMemoryManager from_file(std::string);
+
+private:
+  std::string generate_new_filename(MemoryPurpose);
+  std::string root;
+  nlohmann::json memory_json;
+  nlohmann::json memory_to_overwrite_json;
+  std::uint64_t _cnt = 0;
+};
+
+} // namespace kvaaas
--- src/MemoryManager.cpp
+++ src/MemoryManager.cpp
@@ -1,110 +1,232 @@
 #include "MemoryManager.h"
 #include "ByteArray.h"
-#include <vector>
-#include <optional>
 #include <cassert>
+#include <optional>
+#include <vector>
+
+#include "json.hpp"
 
 namespace kvaaas {
 
-MemoryType::MemoryType(MemoryPurpose mp_, std::optional<std::size_t> sst_level_) :
-    mp(mp_),
-    sst_level(sst_level_) {
-    if (mp_ == MemoryPurpose::SST) {
-        assert(sst_level.has_value());
-    }
+MemoryType::MemoryType(MemoryPurpose mp_, std::optional<std::size_t> sst_level_)
+    : mp(mp_), sst_level(sst_level_) {
+  if (mp_ == MemoryPurpose::SST) {
+    assert(sst_level.has_value());
+  }
 }
 
-MemoryPurpose MemoryType::get_memory_purpose() const {
-    return mp;
-}
+MemoryPurpose MemoryType::get_memory_purpose() const { return mp; }
 
-std::size_t MemoryType::get_sst_level() const {
-    return sst_level.value();
-}
+std::size_t MemoryType::get_sst_level() const { return sst_level.value(); }
 
-bool MemoryType::has_sst_level() const {
-    return sst_level.has_value();
-}
+bool MemoryType::has_sst_level() const { return sst_level.has_value(); }
 
 bool cmp_memory_type(MemoryType memory_type1, MemoryType memory_type2) {
-    return memory_type1.get_memory_purpose() < memory_type2.get_memory_purpose() ||
-          (memory_type1.get_memory_purpose() == memory_type2.get_memory_purpose() &&
-           memory_type1.has_sst_level() && memory_type2.has_sst_level() &&
-           memory_type1.get_sst_level() < memory_type2.get_sst_level());
+  return memory_type1.get_memory_purpose() <
+             memory_type2.get_memory_purpose() ||
+         (memory_type1.get_memory_purpose() ==
+              memory_type2.get_memory_purpose() &&
+          memory_type1.has_sst_level() && memory_type2.has_sst_level() &&
+          memory_type1.get_sst_level() < memory_type2.get_sst_level());
 }
 
-
-ByteArrayPtr RAMMemoryManager::get_file(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) {
-    MemoryType memory_type(memory_purpose, sst_level);
-    return memory.at(memory_type);
+ByteArrayPtr RAMMemoryManager::get_file(MemoryPurpose memory_purpose,
+                                        std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  return memory.at(memory_type);
 }
 
-ByteArrayPtr RAMMemoryManager::create_file(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) {
-    MemoryType memory_type(memory_purpose, sst_level);
-    assert(memory.count(memory_type) == 0);
-    memory[memory_type] = ::new RAMByteArray();
-    return memory[memory_type];
+ByteArrayPtr
+RAMMemoryManager::create_file(MemoryPurpose memory_purpose,
+                              std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  assert(memory.count(memory_type) == 0);
+  memory[memory_type] = ::new RAMByteArray();
+  return memory[memory_type];
 }
 
-ByteArrayPtr RAMMemoryManager::start_overwrite(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) {
-    MemoryType memory_type(memory_purpose, sst_level);
-    assert(memory_to_overwrite.count(memory_type) == 0);
-    memory_to_overwrite[memory_type] = ::new RAMByteArray();
-    return memory_to_overwrite[memory_type];
+ByteArrayPtr
+RAMMemoryManager::start_overwrite(MemoryPurpose memory_purpose,
+                                  std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  assert(memory_to_overwrite.count(memory_type) == 0);
+  memory_to_overwrite[memory_type] = ::new RAMByteArray();
+  return memory_to_overwrite[memory_type];
 }
 
-void RAMMemoryManager::end_overwrite(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) {
-    MemoryType memory_type(memory_purpose, sst_level);
-    delete memory[memory_type];
-    memory[memory_type] = memory_to_overwrite[memory_type];
-    memory_to_overwrite.erase(memory_type);
+void RAMMemoryManager::end_overwrite(MemoryPurpose memory_purpose,
+                                     std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  delete memory[memory_type];
+  memory[memory_type] = memory_to_overwrite[memory_type];
+  memory_to_overwrite.erase(memory_type);
 }
 
-void RAMMemoryManager::remove(MemoryPurpose memory_purpose, std::optional<std::size_t> sst_level) {
-    MemoryType memory_type(memory_purpose, sst_level);
-    delete memory[memory_type];
-    memory.erase(memory_type);
+void RAMMemoryManager::remove(MemoryPurpose memory_purpose,
+                              std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  delete memory[memory_type];
+  memory.erase(memory_type);
 }
 
 ByteArrayPtr RAMMemoryManager::get_file(MemoryPurpose memory_purpose) {
-    MemoryType memory_type(memory_purpose);
-    return memory.at(memory_type);
+  MemoryType memory_type(memory_purpose);
+  return memory.at(memory_type);
 }
 
 ByteArrayPtr RAMMemoryManager::create_file(MemoryPurpose memory_purpose) {
-    MemoryType memory_type(memory_purpose);
-    assert(memory.count(memory_type) == 0);
-    memory[memory_type] = ::new RAMByteArray();
-    return memory[memory_type];
+  MemoryType memory_type(memory_purpose);
+  assert(memory.count(memory_type) == 0);
+  memory[memory_type] = ::new RAMByteArray();
+  return memory[memory_type];
 }
 
 ByteArrayPtr RAMMemoryManager::start_overwrite(MemoryPurpose memory_purpose) {
-    MemoryType memory_type(memory_purpose);
-    assert(memory_to_overwrite.count(memory_type) == 0);
-    memory_to_overwrite[memory_type] = ::new RAMByteArray();
-    return memory_to_overwrite[memory_type];
+  MemoryType memory_type(memory_purpose);
+  assert(memory_to_overwrite.count(memory_type) == 0);
+  memory_to_overwrite[memory_type] = ::new RAMByteArray();
+  return memory_to_overwrite[memory_type];
 }
 
 void RAMMemoryManager::end_overwrite(MemoryPurpose memory_purpose) {
-    MemoryType memory_type(memory_purpose);
-    delete memory[memory_type];
-    memory[memory_type] = memory_to_overwrite[memory_type];
-    memory_to_overwrite.erase(memory_type);
+  MemoryType memory_type(memory_purpose);
+  delete memory[memory_type];
+  memory[memory_type] = memory_to_overwrite[memory_type];
+  memory_to_overwrite.erase(memory_type);
 }
 
 void RAMMemoryManager::remove(MemoryPurpose memory_purpose) {
-    MemoryType memory_type(memory_purpose);
-    delete memory[memory_type];
-    memory.erase(memory_type);
+  MemoryType memory_type(memory_purpose);
+  delete memory[memory_type];
+  memory.erase(memory_type);
 }
 
 RAMMemoryManager::~RAMMemoryManager() noexcept {
-    for (auto [_, ptr] : memory) {
-        delete ptr;
-    }
-    for (auto [_, ptr] : memory_to_overwrite) {
-        delete ptr;
-    }
+  for (auto [_, ptr] : memory) {
+    delete ptr;
+  }
+  for (auto [_, ptr] : memory_to_overwrite) {
+    delete ptr;
+  }
 }
 
+// FileMemoryManager
+
+FileMemoryManager::FileMemoryManager(std::string root) : root(root) {
+
 }
+
+
+FileMemoryManager::FileMemoryManager(nlohmann::json mem_json, nlohmann::json mem_to_ov, std::string root) {
+  
+}
+
+
+std::string FileMemoryManager::generate_new_filename(MemoryPurpose mp) {
+  return root + "/file" + to_string(mp) + std::to_string(_cnt++);
+}
+
+ByteArrayPtr FileMemoryManager::get_file(MemoryPurpose memory_purpose,
+                                         std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  return memory.at(memory_type);
+}
+
+ByteArrayPtr
+FileMemoryManager::create_file(MemoryPurpose memory_purpose,
+                               std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  assert(memory.count(memory_type) == 0);
+  memory[memory_type] =
+      ::new FileByteArray(generate_new_filename(memory_purpose));
+
+  return memory[memory_type];
+}
+
+ByteArrayPtr
+FileMemoryManager::start_overwrite(MemoryPurpose memory_purpose,
+                                   std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  assert(memory_to_overwrite.count(memory_type) == 0);
+  memory_to_overwrite[memory_type] =
+      ::new FileByteArray(generate_new_filename(memory_purpose));
+  return memory_to_overwrite[memory_type];
+}
+
+void FileMemoryManager::end_overwrite(MemoryPurpose memory_purpose,
+                                      std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  delete memory[memory_type];
+  memory[memory_type] = memory_to_overwrite[memory_type];
+  memory_to_overwrite.erase(memory_type);
+}
+
+void FileMemoryManager::remove(MemoryPurpose memory_purpose,
+                               std::optional<std::size_t> sst_level) {
+  MemoryType memory_type(memory_purpose, sst_level);
+  delete memory[memory_type];
+  memory.erase(memory_type);
+}
+
+ByteArrayPtr FileMemoryManager::get_file(MemoryPurpose memory_purpose) {
+  MemoryType memory_type(memory_purpose);
+  return memory.at(memory_type);
+}
+
+ByteArrayPtr FileMemoryManager::create_file(MemoryPurpose memory_purpose) {
+  MemoryType memory_type(memory_purpose);
+  assert(memory.count(memory_type) == 0);
+  memory[memory_type] =
+      ::new FileByteArray(generate_new_filename(memory_purpose));
+  return memory[memory_type];
+}
+
+ByteArrayPtr FileMemoryManager::start_overwrite(MemoryPurpose memory_purpose) {
+  MemoryType memory_type(memory_purpose);
+  assert(memory_to_overwrite.count(memory_type) == 0);
+  memory_to_overwrite[memory_type] =
+      ::new FileByteArray(generate_new_filename(memory_purpose));
+  return memory_to_overwrite[memory_type];
+}
+
+void FileMemoryManager::end_overwrite(MemoryPurpose memory_purpose) {
+  MemoryType memory_type(memory_purpose);
+  delete memory[memory_type];
+  memory[memory_type] = memory_to_overwrite[memory_type];
+  memory_to_overwrite.erase(memory_type);
+}
+
+void FileMemoryManager::remove(MemoryPurpose memory_purpose) {
+  MemoryType memory_type(memory_purpose);
+  delete memory[memory_type];
+  memory.erase(memory_type);
+}
+
+FileMemoryManager::~FileMemoryManager() noexcept {
+  for (auto [_, ptr] : memory) {
+    delete ptr;
+  }
+  for (auto [_, ptr] : memory_to_overwrite) {
+    delete ptr;
+  }
+}
+
+
+static FileMemoryManager from_file(std::string root) {
+  using namespace nlohmann;
+  std::string mem_name = root + "/" + "mememory.json";
+  std::string mem_to_overwrite_name = root + "/" + "mememory_to_overwrite.json";
+ 
+  std::ifstream imem(mem_name);
+  nlohmann::json memory_json;
+  imem >> memory_json;
+
+  imem = std::ifstream(mem_to_overwrite_name);
+  nlohmann::json memory_to_ov_json;
+  imem >> memory_to_ov_json;
+
+
+  return FileMemoryManager(memory_json, memory_to_ov_json,root);
+}
+
+} // namespace kvaaas
