acf384f7d5b1064d0ad4729ef13409edbeeb58cd
Artem Zakharenko <zakhart1234567890@gmail.com>
2022-06-14 19:06:20 +0300

    Fix bug with linear query time

--- include/BloomFilter.h
+++ include/BloomFilter.h
@@ -12,8 +12,8 @@ struct defer {};
 class BloomFilter {
 public:
   explicit BloomFilter(std::size_t elements_cnt)
-      : _function_cnt(std::max(std::size_t(3), elements_cnt + 1)),
-        _seeds(_function_cnt), _data(std::max(std::size_t(10), elements_cnt)) {
+      : _function_cnt(std::max(std::size_t(3), lg(elements_cnt)  + 1)),
+        _seeds(_function_cnt), _data(std::max(std::size_t(10), elements_cnt * C)) {
     std::random_device rd;
     std::mt19937 rng(rd());
     std::uniform_int_distribution<std::mt19937::result_type> dist(0,
@@ -38,6 +38,14 @@ public:
   }
 
 private:
+  static constexpr std::size_t C = 6;
+  std::size_t lg(std::size_t x) {
+      std::size_t res = 0;
+      while (x > (1 << res)) {
+          ++res;
+      }
+      return res;
+  }
   uint32_t hashAt(const KeyType &key, std::size_t index) const noexcept {
     uint32_t current_seed = _seeds[index];
     return XXH32(key.data(), key.size(), current_seed) % _data.size();
